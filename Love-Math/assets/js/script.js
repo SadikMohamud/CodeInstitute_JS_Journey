9227e6f37b7de5e9b088049c0e3300cff4e568a4
SadikMohamud <msadiknur@gmail.com>
2026-01-12 14:29:49 +0000

    Refactor game initialization and enhance runGame function to handle game types

--- Love-Math/assets/js/script.js
+++ Love-Math/assets/js/script.js
@@ -9,9 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
             } else {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked  ${gameType}`);
+                runGame(gameType)
             }
         });
     };
+    runGame("addition");
     
 });
 
@@ -19,10 +21,16 @@ document.addEventListener("DOMContentLoaded", function () {
  * The main game "loop", called when the script is first loaded 
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
     // generate two random numbers between 1 and 25
     let num1 = Math.floor(Math.random() * 25) + 1;
     let num2 = Math.floor(Math.random() * 25) + 1;
+    if(gameType === "addition"){
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unknown game type: ${gameType}`);
+        throw `Unknown game type: ${gameType}. Aborting!`;
+    }
 }
 
 function checkAnswer() {
@@ -37,7 +45,10 @@ function incrementScore() {
 function incrementWrongAnswer() {
 
 }
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
 }
 function displaySubtractQuestion() {
