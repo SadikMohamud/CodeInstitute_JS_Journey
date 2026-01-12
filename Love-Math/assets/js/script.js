313e95794c024e1399593b15f40eb9a8638d81c4
SadikMohamud <msadiknur@gmail.com>
2026-01-12 13:49:20 +0000

    -m setup structure for game functions

---
+++ Love-Math/assets/js/script.js
@@ -0,0 +1,27 @@
+function runGame() {
+    
+}
+function checkAnswer() {
+
+}
+function calculateAnswer() {
+
+}
+function incrementScore() {
+
+}
+function incrementWrongAnswer() {
+
+}
+function displayAdditionQuestion() {
+
+}
+function displaySubtractQuestion() {
+
+}
+function displayMultiplyQuestion() {
+
+}
+function displayDivisionQuestion() {
+    
+}
